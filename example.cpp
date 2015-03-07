89c8d4ac58e3a1b460d16e6342eed142ceccc4c7
jnk0le <jnk0le@hotmail.com>
2015-03-07 21:36:52 +0100

    Revert "update rdm"
    This reverts commit 25b5010421630506618fb688f29557dc3b8d9b85.

--- example.cpp
+++ example.cpp
@@ -7,15 +7,12 @@
 
 int main(void)
 {
-	USART uart0, uart1(BAUD_CALC(115200), 1), serial(BAUD_CALC(19200), 3); // usart0 is set to 9600 baud and 8n1 transmission as default
+	USART uart0, uart1(BAUD_CALC(115200), 1); // usart0 is set to 9600 baud and 8n1 transmission as default
 	sei(); // enable interrupts, library wouldn't work without this
 	
 	uart0.puts("hello from usart 0\r\n"); // write const string to usart buffer 
 	// if you do not have enough SRAM memory space to keep all strings, try to use puts_P instead
 	uart1.puts_P("hello from flashed, usart 1\r\n"); // write string to usart buffer from flash memory
-	
-	serial.puts("hello from quad uarted serial (usart3)\r\n");
-	serial.putfloat(0.1337f);
 	
 	char buffer[13];
 	
