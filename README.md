b1f5c035febb2e0e87142e8be899e5b5ac767e5b
jnk 0le <jnk0le@hotmail.com>
2015-08-05 13:32:16 +0200

    moved class body into bss section

--- README.md
+++ README.md
@@ -9,36 +9,22 @@ buffers for receive/transmit. Designed to be easy to use, especially like arduin
 - optimized as much as possible to reduce code size
 - and much more
 
-int main(void)
-{
-	USART uart0;
-    sei();
-    while(1)
-    {
-		uart0.puts("hello world\r\n");
-		_delay_ms(1000);
-    }
-}
-
-
-
-gives
+simple "hello world" gives
 
          text      data       bss       dec       hex   filename
-          502        14         2       518       206   avrtpp.elf
+          474        16        70       560       230   avrtpp.elf
    
-Program Memory Usage    :   516 bytes   1,6 % Full
-Data Memory Usage       :   16 bytes   0,5 % Full
+Program Memory Usage    :   490 bytes   1,5 % Full
+Data Memory Usage       :   86 bytes   4,2 % Full
 
 defined  NO_USART_RX flag gives
 
          text      data       bss       dec       hex   filename
-          412        14         2       428       1ac   avrtpp.elf
+          390        16        36       442       1ba   avrtpp.elf
 
-Program Memory Usage    :   426 bytes   1,3 % Full
-Data Memory Usage       :   16 bytes   0,5 % Full
+Program Memory Usage    :   406 bytes   1,2 % Full
+Data Memory Usage       :   52 bytes   2,5 % Full
 
-Of course the real memory usage is higher by buffers sizes and some variables.
 
 
 
--- example.cpp
+++ example.cpp
@@ -4,17 +4,22 @@
 
 #include "usart.hpp"
 
+USART uart0, uart1, serial;
 
 int main(void)
 {
-	USART uart0, uart1(BAUD_CALC(115200), 1), serial(BAUD_CALC(19200), 3); // usart0 is set to 9600 baud and 8n1 transmission as default
+	uart0.init(); // default 9600kbps 
+	uart1.init(BAUD_CALC(115200), 1);
+	
+	serial.begin(BAUD_CALC(19200), 3);
+	
 	sei(); // enable interrupts, library wouldn't work without this
 	
 	uart0.puts("hello from usart 0\r\n"); // write const string to usart buffer 
 	// if you do not have enough SRAM memory space to keep all strings, try to use puts_P instead
 	uart1.puts_P("hello from flashed, usart 1\r\n"); // write string to usart buffer from flash memory
 	
-	serial.puts("hello from quad uarted serial (usart3)\r\n");
+	serial.puts("fake arduino serial coming soon\r\n");
 	serial.putfloat(0.1337f);
 	
 	char buffer[13];
@@ -23,10 +28,10 @@ int main(void)
 	{
 		uart0.puts("bytes waiting in buffer : ");
 		uart0.putint(uart0.AvailableBytes()); // ask for bytes waiting in receiver buffer
-		uart0.gets(buffer, 13); // read 13 bytes or one line from usart buffer
+		uart0.getln(buffer, 13); // read 12 bytes or one line from usart buffer
 		uart0.puts("/r/n");
 		
-		uart1.putstr(buffer); // write dynamic string to usart buffer
+		uart1.putstr(buffer); // write string array to usart buffer
 		_delay_ms(5000);
 	}
 }
